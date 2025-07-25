ea989fe6c37d53896686c6360d8d356b943a45df
cieren edinger <cierenedinger1@gmail.com>
2025-07-25 16:04:38 +1000

    Basic setup

--- ConsoleApplication2.cpp
+++ ConsoleApplication2.cpp
@@ -1,9 +1,95 @@
 // ConsoleApplication2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#define STB_IMAGE_IMPLEMENTATION
 
 #include <iostream>
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include "stb_image.h"  
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Initialistion boilerplate
+    if (glfwInit() == NULL)
+    {
+        std::cout << "Window initialisation bungled mate" << std::endl;
+        return -1;
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "2D Renderer", nullptr, nullptr);
+    if (window == NULL)
+    {
+        std::cout << "Window create fail!" << std::endl;
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    //this piece of code allows us to use OpenGL functions
+    //only call once in the main code
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    std::cout << glGetString(GL_VERSION) << std::endl; //printing the version 
+
+    /*
+    GLint nrAttributes;
+    glGetIntegerv(GL_MAX_VERTEX_ATTRIBS, &nrAttributes);
+    std::cout << "Maximum nr of vertex attributes supported: " << nrAttributes << std::endl;
+    */
+
+    //viewport
+    unsigned int viewport_width = 800;
+    unsigned int viewport_height = 600;
+    glViewport(0, 0, viewport_width, viewport_height);
+
+    //glfw callbacks
+    glfwSetKeyCallback(window, key_callback);
+    //glfwSetCursorPosCallback(window, mouse_callback);
+    //glfwSetScrollCallback(window, scroll_callback);
+    //glfwSetWindowSizeCallback(window, framebuffer_size_callback);
+
+    // Hide and capture cursor when application has focus
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // Render loop for now
+    while (!glfwWindowShouldClose(window))
+    {
+        glClearColor(0.4, 0.75, 0.60, 1.0);
+        //glClearColor(0.235, 0.701, 0.443, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    // When a user presses the escape key, we set the WindowShouldClose property to true, // closing the application
+
+    // Utility
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    /*
+        void keyCallback (GLFWwindow\* wind, int key, int scancode, int action, int mods) {
+        my_input_class.keyCallback(wind, key, scancode, action, mods);
+        }
+    */
 }
